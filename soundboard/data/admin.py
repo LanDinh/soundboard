c18b3bff521ea0d78da841b1712b2cb38a7e3cde
Daniel Stelter-Gliese <ds@siriuswhite.de>
2022-04-17 17:36:13 +0200

    Validate file extension and infer file type

--- soundboard/data/admin.py
+++ soundboard/data/admin.py
@@ -4,4 +4,9 @@ from .models import Origin, Sound
 
 # Register your models here.
 admin.site.register(Origin)
-admin.site.register(Sound)
+
+class SoundAdmin(admin.ModelAdmin):
+    readonly_fields = ('type',)
+
+
+admin.site.register(Sound, SoundAdmin)
--- soundboard/data/models.py
+++ soundboard/data/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -15,15 +16,47 @@ class Locale:
     ]
 
 
+class FileType:
+    MP3 = 'audio/mp3'
+    WAV = 'audio/wav'
+
+    choices = [
+        (MP3, MP3),
+        (WAV, WAV),
+    ]
+
+    def from_filename(name):
+        if name.endswith('.mp3'):
+            return FileType.MP3
+        elif name.endswith('.wav'):
+            return FileType.WAV
+        else:
+            return None
+
 
 class Origin(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+def validate_file(file):
+    if FileType.from_filename(file.name) is None:
+        raise ValidationError('Unsupported file type')
 
 
 class Sound(models.Model):
     origin = models.ForeignKey(Origin, on_delete=models.CASCADE, related_name='sounds')
     name = models.CharField(max_length=1000)
-    file = models.FileField(upload_to=get_sound_file_path)
-    type = models.CharField(max_length=100)
+    file = models.FileField(upload_to=get_sound_file_path, validators=[validate_file])
+    type = models.CharField(max_length=30, choices=FileType.choices)
     transcription = models.TextField(blank=True)
     language = models.CharField(max_length=10, choices=Locale.choices)
+
+    def save(self, *args, **kwargs):
+        self.type = FileType.from_filename(self.file.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.file.name)
