bd5e144681a1026c845caf2057a5a4741e4c6d1a
Daniel Stelter-Gliese <ds@siriuswhite.de>
2022-04-17 18:50:26 +0200

    Basic edit form for sounds
    Along with some JS to make quick navigation easier.

--- soundboard/data/views.py
+++ soundboard/data/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
+from .forms import SoundEditForm
 from .models import Sound, Origin
 
 # Create your views here.    
@@ -12,6 +14,14 @@ class SoundListView(ListView):
     
     def get_queryset(self):
         return Origin.objects.prefetch_related('sounds').all().order_by('name')
+
+    def post(self, request):
+        sound = Sound.objects.get(pk=request.POST.get('id'))
+        form = SoundEditForm(request.POST, instance=sound)
+        if not form.is_valid():
+            raise ValidationError(form.errors.as_json())
+        form.save()
+        return HttpResponseRedirect('/data#next-{}'.format(sound.pk))
         
 def get_sound(request, id):
     sound = Sound.objects.get(id=id)
