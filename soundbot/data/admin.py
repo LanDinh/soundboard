ccbfa06cf77d72aca4f58c04f6f497b87c0e271f
LanDinh <coding+sourcetree@khaleesi.ninja>
2022-04-17 14:09:57 +0200

    First version

---
+++ soundbot/data/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Origin, Sound
+
+# Register your models here.
+admin.site.register(Origin)
+admin.site.register(Sound)
---
+++ soundbot/data/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+def get_sound_file_path(instance, filename):
+    return f'data/{instance.origin.name}/{instance.language}/{filename}'
+    
+    
+class Locale:
+    EN_US = 'en-US'
+    DE_DE = 'de-DE'
+    
+    choices = [
+        (EN_US, EN_US),
+        (DE_DE, DE_DE),
+    ]
+
+
+
+class Origin(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Sound(models.Model):
+    origin = models.ForeignKey(Origin, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000)
+    file = models.FileField(upload_to=get_sound_file_path)
+    type = models.CharField(max_length=100)
+    transcription = models.TextField(blank=True)
+    language = models.CharField(max_length=10, choices=Locale.choices)
